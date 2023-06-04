4edef802a28e120f63eb8d442212f7cc4261e770
taeheoki <dkqjwl2365@naver.com>
2023-06-04 20:13:53 +0900

    🧑🏻‍💻feat: pdf 파일 업로드 구현

---
+++ src/main/java/com/fastcampus05/zillinks/core/util/model/s3upload/S3UploaderFile.java
@@ -0,0 +1,2 @@
+package com.fastcampus05.zillinks.core.util.model.s3upload;public class S3Uploader {
+}
---
+++ src/main/java/com/fastcampus05/zillinks/core/util/model/s3upload/S3UploaderFileRepository.java
@@ -0,0 +1,2 @@
+package com.fastcampus05.zillinks.core.util.model.s3upload;public interface S3UploaderFileRepository {
+}
