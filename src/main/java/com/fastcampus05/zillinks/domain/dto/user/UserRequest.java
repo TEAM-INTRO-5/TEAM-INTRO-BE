3faa9667c914e788b440251aca005d800fdcd7b1
taeheoki <dkqjwl2365@naver.com>
2023-06-11 21:03:06 +0900

    🚑fix: 비밀번호 재설정 시 기존 비밀번호 검증 추가

--- src/main/java/com/fastcampus05/zillinks/domain/dto/user/UserRequest.java
+++ src/main/java/com/fastcampus05/zillinks/domain/dto/user/UserRequest.java
@@ -52,9 +52,13 @@ public class UserRequest {
 
     @Getter
     public static class UpdatePasswordInDTO {
-        @Schema(description = "패스워드", example = "0ss123erljow!")
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=(.*\\d)|(.*\\W)).{10,}$", message = "10자 이상 입력, 영문/숫자/특수문자(공백 제외) 2개 이상 조합해야합니다.")
+        @Schema(description = "기존 비밀번호", example = "0ss123erljow!")
         private String password;
+
+        @Schema(description = "새 비밀번호", example = "0ss123erljow!")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=(.*\\d)|(.*\\W)).{10,}$", message = "10자 이상 입력, 영문/숫자/특수문자(공백 제외) 2개 이상 조합해야합니다.")
+        @JsonProperty("new_password")
+        private String newPassword;
     }
 
     @Getter
