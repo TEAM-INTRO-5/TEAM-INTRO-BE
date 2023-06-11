97d958d08c314d16a50e56e21e9565c81fe69523
taeheoki <dkqjwl2365@naver.com>
2023-06-11 18:32:11 +0900

    ⭐️style: IntroPageLog 생성

---
+++ src/main/java/com/fastcampus05/zillinks/domain/model/log/intropage/ContactUsLogRepository.java
@@ -0,0 +1,6 @@
+package com.fastcampus05.zillinks.domain.model.log.intropage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactUsLogRepository extends JpaRepository<ContactUsLog, Long> {
+}
---
+++ src/main/java/com/fastcampus05/zillinks/domain/model/log/intropage/ContactUsStatus.java
@@ -0,0 +1,5 @@
+package com.fastcampus05.zillinks.domain.model.log.intropage;
+
+public enum ContactUsStatus {
+    CANCEL, CONFIRM
+}
