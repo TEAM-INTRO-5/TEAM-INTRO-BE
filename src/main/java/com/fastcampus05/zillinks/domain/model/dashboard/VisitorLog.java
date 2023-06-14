b7156cc786b5de2a50cfbd8fd0b81ba0e4e36615
taeheoki <dkqjwl2365@naver.com>
2023-06-14 16:34:59 +0900

    ⭐️style: visitor 관련 엔티티 생성

--- src/main/java/com/fastcampus05/zillinks/domain/model/dashboard/VisitorLog.java
+++ src/main/java/com/fastcampus05/zillinks/domain/model/dashboard/VisitorLog.java
@@ -1,22 +1,31 @@
 package com.fastcampus05.zillinks.domain.model.dashboard;
 
+import com.fastcampus05.zillinks.core.util.TimeBaseEntity;
+import com.fastcampus05.zillinks.domain.model.intropage.IntroPage;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-@Getter
 @Entity
+@Getter
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "dtype")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@DiscriminatorValue("visitor_log")
 @Table(name = "visitor_log_tb")
-public class VisitorLog extends Dashboard {
+public abstract class VisitorLog extends TimeBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dashboard_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "intro_page_id")
+    private IntroPage introPage;
+
     @NotNull
+    @Column(name = "device_type")
     private String deviceType;
-    @NotNull
-    private String keyword;
 }
---
+++ src/main/java/com/fastcampus05/zillinks/domain/model/dashboard/repository/SharingLogRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus05.zillinks.domain.model.dashboard.repository;
+
+import com.fastcampus05.zillinks.domain.model.dashboard.SharingLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SharingLogRepository extends JpaRepository<SharingLog, Long> {
+}
