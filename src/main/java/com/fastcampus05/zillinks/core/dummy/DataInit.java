07802c036b4f78582bd5645f4d1976e90bc62edc
taeheon <87412682+taeheoki@users.noreply.github.com>
2023-06-22 22:19:58 +0900

    ♻️refactor: uploader 파일 경로 변경 (#110)

--- src/main/java/com/fastcampus05/zillinks/core/dummy/DataInit.java
+++ src/main/java/com/fastcampus05/zillinks/core/dummy/DataInit.java
@@ -16,7 +16,16 @@ public class DataInit extends DummyEntity{
     @Bean
     CommandLineRunner init(UserRepository userRepository, IntroPageRepository introPageRepository, S3UploaderFileRepository s3UploaderFileRepository){
         return args -> {
-            User taeheoki = userRepository.save(newUser("taeheoki", "2258701327"));
+            User taeheoki = userRepository.save(newUser("test1", "2258701327"));
+            userRepository.save(newUser("test2", "1248234535"));
+            userRepository.save(newUser("test3", "7318200018"));
+            userRepository.save(newUser("test4", "4668600994"));
+            userRepository.save(newUser("test5", "6768700400"));
+            userRepository.save(newUser("test6", "7748101583"));
+            userRepository.save(newUser("test7", "4598100313"));
+            userRepository.save(newUser("test8", "3108133203"));
+            userRepository.save(newUser("test9", "2778701537"));
+            userRepository.save(newUser("test10", "5868802285"));
         };
     }
 }
