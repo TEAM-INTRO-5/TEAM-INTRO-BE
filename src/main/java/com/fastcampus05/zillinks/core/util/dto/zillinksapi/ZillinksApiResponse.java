748d0470cdbcc05e21d771e407c7a252997613c7
taeheon <87412682+taeheoki@users.noreply.github.com>
2023-06-30 17:43:39 +0900

    🚑fix: 질링스 Response 필드 추가 (#125)

--- src/main/java/com/fastcampus05/zillinks/core/util/dto/zillinksapi/ZillinksApiResponse.java
+++ src/main/java/com/fastcampus05/zillinks/core/util/dto/zillinksapi/ZillinksApiResponse.java
@@ -3,22 +3,28 @@ package com.fastcampus05.zillinks.core.util.dto.zillinksapi;
 import com.fastcampus05.zillinks.domain.model.intropage.ZillinksData;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Getter
+@Setter
 public class ZillinksApiResponse {
 
     private Query query;
     private Result result;
 
     @Getter
+    @Setter
     public static class Query {
         private String bizNum;
     }
 
     @Getter
+    @Setter
     public static class Result {
         private String name;                           // 기업명
         private Address address;                       // 기업 주소
@@ -26,7 +32,7 @@ public class ZillinksApiResponse {
         private String intro;                          // 기업 소개글
         private String logo;                           // 로고 주소
         private String representative;                 // 대표자
-        private List<String> tags;                     // 기업 키워드
+        private List<String> tags = Collections.emptyList();    // 기업 키워드 (Optional)
         private List<History> history;                 // 기업 연혁
         private List<Team> teams;                      // 팀 정보
         private String regNum;
@@ -35,23 +41,25 @@ public class ZillinksApiResponse {
         @JsonProperty("isCorporation")
         private boolean isCorporation;                 // 법인 여부
         @JsonProperty("startDate")
-        private String startDate;                      // 설립일
+        private String startDate;                          // 설립일 (Optional)
         @JsonProperty("introFile")
         private String introFile;                      // 소개 파일
         @JsonProperty("mainBackground")
         private String mainBackground;                 // 기업 메인 배경 이미지 주소
         @JsonProperty("tagline")
-        private String tagline;                        // 기업 한줄 소개
+        private String tagline;                            // 기업 한줄 소개 (Optional)
         @JsonProperty("bizArea")
-        private List<String> bizArea;                  // 산업 분야
+        private List<String> bizArea = Collections.emptyList(); // 산업 분야 (Optional)
         @JsonProperty("bizStatus")
         private String bizStatus;                      // 휴폐업 상태
         @JsonProperty("contactEmail")
         private String contactEmail;                   // 담당자 이메일
         private Employment employment;                 // 고용 정보
+        private String nationality;                    // 국적
     }
 
     @Getter
+    @Setter
     public static class Address {
         private String zonecode;          // 추가된 zonecode 필드
         private String sido;              // 시도
@@ -61,12 +69,14 @@ public class ZillinksApiResponse {
     }
 
     @Getter
+    @Setter
     public static class History {
         private String date;             // 날짜
         private String title;            // 제목
     }
 
     @Getter
+    @Setter
     public static class Team {
         private String name;             // 팀원 이름
         private String position;         // 부서
@@ -82,6 +92,7 @@ public class ZillinksApiResponse {
     }
 
     @Getter
+    @Setter
     public static class Employment {
         @JsonProperty("latestStaffCount")
         private int latestStaffCount;                  // 최근 직원수
